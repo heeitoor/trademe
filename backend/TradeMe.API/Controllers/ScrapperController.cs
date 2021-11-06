ffbfb6ca960d6b5364ea2bc7d8a1142f9abb93fe
unknown <heitordesousa@gmail.com>
2021-11-06 14:25:39 -0300

    init

---
+++ backend/TradeMe.API/Controllers/ScrapperController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TradeMe.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScrapperController : ControllerBase
+    {
+        private readonly ILogger<ScrapperController> _logger;
+
+        public ScrapperController(ILogger<ScrapperController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "";
+        }
+    }
+}
